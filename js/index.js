39d358779cf658a7bce6faf80a0c72c8fd6f570f
Dylan Vasquez <developercoaching@proyeccionhumana.org>
2026-06-18 11:29:43 -0500

    First commit

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 const components = {
-  navbar: '../sections/components/navbar.html',
-  hero: '../sections/components/hero.html',
-  statics: '../sections/components/statics.html',
-  benefits: '../sections/components/benefits.html',
-  receive: '../sections/components/receive.html',
-  contexts: '../sections/components/contexts.html',
-  modules: '../sections/components/modules.html',
-  footer: '../sections/components/footer.html'
+  navbar: './sections/components/navbar/navbar.html',
+  hero: './sections/components/hero/hero.html',
+  statics: './sections/components/statics/statics.html',
+  benefits: './sections/components/benefits/benefits.html',
+  receive: './sections/components/receive/receive.html',
+  contexts: './sections/components/contexts/contexts.html',
+  modules: './sections/components/modules/modules.html',
+  footer: './sections/components/footer/footer.html'
 };
 
 document.addEventListener('DOMContentLoaded', async () => {
